3d644873259189400195d634430115bd438ef41c
Vincent Bernardoff <vb@luminar.eu.org>
2013-06-01 15:47:47 +0100

    Fixed sdbtest.ml

--- test/sdbtest.ml
+++ test/sdbtest.ml
@@ -2,58 +2,50 @@ open Lwt
 
 open SDB
 
-let creds = { Creds.aws_access_key_id = Keys.k; 
-              Creds.aws_secret_access_key = Keys.c } 
- 
-let list_domains () = 
-  Lwt_main.run 
-    (SDB.list_domains creds ()
-     >>= function 
-       | `Ok (l,_) -> List.iter print_endline l ; return ()
-       | `Error (code, msg) -> Printf.printf "Panic: %s\n" msg; return ()) 
+let creds = Creds.({ aws_access_key_id     = Keys.k;
+                     aws_secret_access_key = Keys.c })
 
-let create_domain name = 
-  Lwt_main.run 
-    (SDB.create_domain creds name
-     >>= function 
-       | `Ok -> return ()
-       | `Error (code, msg) -> Printf.printf "Panic: %s\n" msg; return ())
+let list_domains () =
+  SDB.list_domains creds () >>= function
+  | `Ok (l,_) -> List.iter print_endline l ; return ()
+  | `Error (code, msg) -> Lwt_io.printf "Panic: %s\n" msg
 
-let delete_domain name = 
-  Lwt_main.run 
-    (SDB.delete_domain creds name
-     >>= function 
-       | `Ok -> return ()
-       | `Error (code, msg) -> Printf.printf "Panic: %s\n" msg; return ())
+let create_domain name =
+  SDB.create_domain creds name >>= function
+  | `Ok -> return ()
+  | `Error (code, msg) -> Lwt_io.printf "Panic: %s\n" msg
 
-let get_attributes domain item = 
-   Lwt_main.run 
-     (SDB.get_attributes ~encoded:false creds ~domain ~item ()
-      >>= function 
-        | `Ok l -> List.iter (fun (n, v) -> 
-          match v with None -> () | Some v -> Printf.printf "%s -> %s\n" n v) l; return ()
-       | `Error (code, msg) -> Printf.printf "Panic: %s\n" msg; return ())
+let delete_domain name =
+  SDB.delete_domain creds name >>= function
+  | `Ok -> return ()
+  | `Error (code, msg) -> Lwt_io.printf "Panic: %s\n" msg
 
-let rec select token expr = 
-  SDB.select ~encoded:false ~token creds expr
-  >>= function 
-    | `Ok (l, nxt) ->
-         List.iter (fun (name, attrs) -> 
-           print_endline name ;
-           List.iter (fun (name, value) -> Printf.printf "   %s -> %s\n" name (match value with Some s -> s | None -> "none")) attrs) l;       
-      (match nxt with 
-          None -> Printf.printf "no token\n"; return () 
-        | Some _ -> select nxt expr)
-    | `Error (code, msg) -> Printf.printf "Panic: %s\n" msg; return ()
-    
-(* 
-   let _ = 
-   list_domains () ; 
-   let domain = Sys.argv.(1) in 
-   delete_domain domain ; 
-   create_domain domain
-*)
+let get_attributes domain item =
+  SDB.get_attributes ~encoded:false creds ~domain ~item () >>= function
+  | `Ok l -> Lwt_list.iter_s (fun (n, v) ->
+    match v with
+      | None -> Lwt.return ()
+      | Some v -> Lwt_io.printf "%s -> %s\n" n v) l
+  | `Error (code, msg) -> Lwt_io.printf "Panic: %s\n" msg
 
-let _ = 
-  Lwt_main.run 
-    (select None Sys.argv.(1))
+let rec select token expr =
+  SDB.select ~encoded:false ~token creds expr >>= function
+  | `Ok (l, nxt) ->
+    List.iter (fun (name, attrs) ->
+      print_endline name;
+      List.iter (fun (name, value) -> Printf.printf "   %s -> %s\n" name (match value with Some s -> s | None -> "none")) attrs) l;       
+    (match nxt with
+        None -> Lwt_io.printf "no token\n"
+      | Some _ -> select nxt expr)
+  | `Error (code, msg) -> Lwt_io.printf "Panic: %s\n" msg
+
+
+let _ =
+  list_domains ()
+  (* let domain = Sys.argv.(1) in  *)
+  (* delete_domain domain ;  *)
+  (* create_domain domain *)
+
+(* let _ =  *)
+(*   Lwt_main.run  *)
+(*     (select None Sys.argv.(1)) *)
